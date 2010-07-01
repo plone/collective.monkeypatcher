b14367c683e3a4bb8040194cc90ebafdf87cffe2
Hanno Schlichting <hanno@hannosch.eu>
2010-07-01 15:49:59 +0000

    Prepare collective.monkeypatcher 1.0.
    svn path=/collective.monkeypatcher/trunk/; revision=120063

--- setup.py
+++ setup.py
@@ -1,16 +1,14 @@
 # -*- coding: utf-8 -*-
-# $Id$
 from setuptools import setup, find_packages
 import os
 
-version = '1.0b3'
+version = '1.0'
 
 setup(name='collective.monkeypatcher',
       version=version,
       description="Support for applying monkey patches late in the startup cycle by using ZCML configuration actions",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Zope2 :: Zope3",
         "Programming Language :: Python",
@@ -28,6 +26,4 @@ setup(name='collective.monkeypatcher',
       install_requires=[
           'setuptools',
       ],
-      entry_points="""
-      """,
       )

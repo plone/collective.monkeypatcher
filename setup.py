bd4eb1a1d57beb80096f6e4bbd7a9c93e5cd91b6
Maurits van Rees <maurits@vanrees.org>
2016-02-09 22:22:40 +0100

    Added classifiers in setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -8,18 +8,20 @@ version = '1.1.2.dev0'
 setup(
     name='collective.monkeypatcher',
     version=version,
-    description="Support for applying monkey patches late in the startup \
-        cycle by using ZCML configuration actions",
+    description=("Support for applying monkey patches late in the startup "
+                 "cycle by using ZCML configuration actions"),
     long_description='\n'.join([
         open("README.rst").read(),
         open("CHANGES.rst").read()]),
     classifiers=[
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
         ],
     keywords='zope monkey patch',
     author='Martin Aspeli',
     author_email='optilude@gmail.com',
-    url='http://pypi.python.org/pypi/collective.monkeypatcher',
+    url='https://pypi.python.org/pypi/collective.monkeypatcher',
     license='BSD',
     packages=find_packages(exclude=['ez_setup']),
     namespace_packages=['collective'],

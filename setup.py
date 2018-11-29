3b8d8f9f8a4beae4d2d54d1bf4e1d3a6c595e88b
Michael Howitz <mh@gocept.com>
2018-11-29 17:11:46 +0100

    Remove no longer supported version. It is untested.

--- setup.py
+++ setup.py
@@ -16,7 +16,6 @@ setup(
     classifiers=[
         "License :: OSI Approved :: BSD License",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
